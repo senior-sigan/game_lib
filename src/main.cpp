a1b4823f71223400010b350f70ce15369d7291a7
Ilya Siganov <ilya.blan4@gmail.com>
2020-04-18 15:14:12 +0600

    init game

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,34 @@ void UpdateDrawFrame() {
 
 static bool run() {
   JsRuntimeHolder js(script_path);
-  Context ctx{};
+  Context ctx(
+      128, 128,
+      {
+          {"up", KEY_UP},
+          {"down", KEY_DOWN},
+          {"left", KEY_LEFT},
+          {"right", KEY_RIGHT},
+          {"start", KEY_ENTER},
+          {"pause", KEY_P},
+          {"space", KEY_SPACE},
+          {"z", KEY_Z},
+          {"x", KEY_X},
+      },
+      {
+          {{0, 0, 0, 255},
+              {43, 40, 33, 255},
+              {98, 76, 60, 255},
+              {217, 172, 139, 255},
+              {227, 207, 180, 255},
+              {36, 61, 92, 255},
+              {93, 114, 117, 255},
+              {92, 139, 147, 255},
+              {177, 165, 141, 255},
+              {176, 58, 72, 255},
+              {212, 128, 77, 255},
+              {224, 200, 114, 255},
+              {62, 105, 88, 255}}
+      });
   SetContext(&ctx);
 
   GetContext()->Init(&js);
